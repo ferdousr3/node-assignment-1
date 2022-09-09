72c1fc9181af88ebe02424d0a0a4f154aa7bcbad
ferdousr3 <ferdousr1992@gmail.com>
2022-09-10 02:03:27 +0600

    all done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,10 @@ const fsPromises = require("fs").promises;
 const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
-const { saveNewUser, getUserData } = require("../middleware/user");
+const { saveNewUser, getUserData } = require("../utility/user");
 const user = require("../data/data.json");
-const users = require("../data/user.json");
 
-// get one  random user
+// get a random user
 const getUserRandom = async (req, res) => {
   try {
     const data = await fsPromises.readFile(
@@ -29,14 +28,13 @@ const getAllUsers = async (req, res) => {
       path.join(__dirname, "../data", "data.json"),
       "utf8"
     );
-    const newsData = await JSON.parse(data);
-    res.status(200).json(newsData);
+    const newData = await JSON.parse(data);
+    const randomUsers = newData.sort(() => 0.5 - Math.random());
+    res.status(200).json(randomUsers);
   } catch (err) {
-    res.status(500).send(error.message);
+    res.status(500).send(err.message);
   }
 };
-
-
 
 // save a user
 const saveUser = async (req, res) => {
@@ -75,4 +73,108 @@ const saveUser = async (req, res) => {
   }
 };
 
-module.exports = { getAllUsers, getUserRandom, saveUser };
+// update a user
+const updateUser = async (req, res) => {
+  try {
+    const userId = req.body.id;
+    //get the update data
+    const userData = req.body;
+    //get the existing user
+    const existUsers = getUserData();
+    //check if the user id is missing
+    if (userId == null) {
+      return res.status(401).send({ error: true, msg: "User Id is missing" });
+    }
+    //check if the user id exist or not
+    const findExist = existUsers.find((user) => user.id === userId);
+    if (!findExist) {
+      return res.status(409).send({ error: true, msg: "user id not exist" });
+    }
+
+    //filter the user to remove it
+    const updateUser = existUsers.filter((user) => user.id !== userId);
+    //push the updated data
+    updateUser.push(userData);
+    //save the filtered
+    saveNewUser(updateUser);
+    res.send({ success: true, msg: "User update successfully" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// update multiple  user
+const bulkUpdate = async (req, res) => {
+  try {
+    const userId = req.body.map((item) => item.id);
+    //get the update data
+    const userData = req.body;
+    //get the existing user
+    const existUsers = getUserData();
+    //check if the user id is missing
+  //  const missingFiled = userData.filter((x) => !existUsers.includes(x));
+  //  const isSameUser = (missingFiled, userData) =>
+  //    userData.value === existUsers.value &&
+  //    existUsers.name === userData.name;
+
+  //   console.log('missing',missingFiled);
+  //   if (missingFiled == null) {
+  //     return res.status(401).send({ error: true, msg: "User Id is missing" });
+  //   }
+    //check if the user id exist or not
+
+    const keys = existUsers.map((user) => user.id);
+    const isExist = userId.every((id) => keys.includes(id));
+
+    if (isExist === false) {
+      return res.status(409).send({ error: true, msg: "user id not exist" });
+    }
+
+    const updatedUser = existUsers.map(
+      (user) => userData.find((item) => item.id === user.id) || user
+    );
+    
+
+    //push the updated data
+
+    saveNewUser(updatedUser);
+    res.send({ success: true, msg: "User update successfully" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+// delete a user
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.body.id;
+    console.log(userId);
+    //get the existing user
+    const existUsers = getUserData();
+    //check if the user id is missing
+    if (userId == null) {
+      return res.status(401).send({ error: true, msg: "User Id is missing" });
+    }
+    //filter the user to remove it
+    const filterUser = existUsers.filter((user) => user.id !== userId);
+    if (existUsers.length === filterUser.length) {
+      return res
+        .status(409)
+        .send({ error: true, msg: "user id does not exist" });
+    }
+    //save the filtered
+    saveNewUser(filterUser);
+    res.send({ success: true, msg: "User removed successfully" });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserRandom,
+  saveUser,
+  updateUser,
+  bulkUpdate,
+  deleteUser,
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const { getAllUsers, getUserRandom, saveUser } = require("../controllers/user.controller");
+const {
+  getAllUsers,
+  getUserRandom,
+  saveUser,
+  deleteUser,
+  updateUser,
+  bulkUpdate,
+} = require("../controllers/user.controller");
 const router = express.Router();
 const bodyParser = require("body-parser");
 
 router.get("/random", getUserRandom);
 router.get("/all", getAllUsers);
 router.post("/save", bodyParser.json(), saveUser);
-
+router.patch("/update", bodyParser.json(), updateUser);
+router.patch("/bulk-update", bodyParser.json(), bulkUpdate);
+router.delete("/delete", bodyParser.json(), deleteUser);
 
 module.exports = router;
